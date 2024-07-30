326b32acdc71584ede41d51e84983e20051cee84
ngdx4 <ngdx04@gmail.com>
2024-07-30 09:00:36 -0300

    xadrez_att2

--- xadrez/xadrez.c
+++ xadrez/xadrez.c
@@ -4,16 +4,6 @@
 #include<ctype.h>
 #include<unistd.h>
 
-
-typedef struct{
-	char peao[10];
-	char torre[10];
-	char cavalo[10];
-	char bispo[10];
-	char rei;
-	char rainha[10];
-} pecas;
-
 typedef char criar[9][9];
 
 void clearbuffer (){
@@ -23,65 +13,31 @@ void clearbuffer (){
 
 int menu (){
 	char saida[10];
-	int i = 0;
-	while(saida){
-//		system("clear");
-		printf("BEM VINDO AO JOGO DE XADREZ\n\n");
+	while(!strstr(saida, "sair")){
 		printf("Escreva uma da opções abaixo:\n");
-		printf("Jogar - para iniciar uma partida\n");
+		printf("Jogar - para iniciar uma nova partida\n");
 		printf("Sair - para fechar o jogo\n\n");
-		fgets(saida,10,stdin);
+		if(fgets(saida, sizeof(saida), stdin) == NULL){
+			printf("ERRO DE LEITURA\n");
+			clearbuffer();
+			continue;
+		}
 		for(int i=0; i<strlen(saida); i++){
 			saida[i] = tolower(saida[i]);
 		}
-		//clearbuffer();
 		if(strstr(saida, "jogar")){
 			return 1;
 		}else if(strstr(saida, "sair")){
-			return 0;
+			break;
 		}else{
-			printf("Escolha não reconhecida, tente novamente\n");
-		}
-		i+=1;
-		if(i == 10){ // em caso de erro de leitura fechar o programa
-			printf("ERRO\n");
-			return 0; 
+			printf("Escolha não reconhecida, digite novamente\n");
 		}
 	}
-	
+	return 0;
 }
-void definir_pecas(pecas  *pb, pecas *pp){
 
+void organizar_tabuleiro(criar tabuleiro){
 	char pecas_tab[] = {'P','T','H','B','R','Q'};
-	enum contagem{
-		pawn,rook,knight,bishop,king,queen
-	};
-	for(int i=0; i<=5;i++){
-		for(int j=0;j<10;j++){
-			if(i == pawn){
-				pb->peao[j] = pecas_tab[i];
-				pp->peao[j] = tolower(pecas_tab[i]);
-			}else if(i == rook){
-				pb->torre[j] = pecas_tab[i];
-				pp->torre[j]= tolower(pecas_tab[i]);
-			}else if(i == knight){
-				pb->cavalo[j] = pecas_tab[i];
-				pp->cavalo[j] = tolower(pecas_tab[i]);
-			}else if(i == bishop){
-				pb->bispo[j] = pecas_tab[i];
-				pp->bispo[j] = tolower(pecas_tab[i]);
-			}else if(i == king){
-				pb->rei = pecas_tab[i];
-				pp->rei = tolower(pecas_tab[i]);
-			}else if(i == queen){
-				pb->rainha[j] = pecas_tab[i];
-				pp->rainha[j] = tolower(pecas_tab[i]);
-			}
-		}
-	}
-}
-
-void organizar_tabuleiro(criar tabuleiro,pecas *pb, pecas *pp){
 	char num[9] = {' ','1','2','3','4','5','6','7','8'};
 	char letras[8] = {'a','b','c','d','e','f','g','h'};
 	for(int i=0; i<9;i++){
@@ -93,97 +49,280 @@ void organizar_tabuleiro(criar tabuleiro,pecas *pb, pecas *pp){
 			}else if(i == 0 && j == 8){
 				tabuleiro[i][j]	= '\n';
 			}else if(i == 1){
-				tabuleiro[i][1] = pp->torre[0];
-				tabuleiro[i][8] = pp->torre[1];
-				tabuleiro[i][2] = pp->cavalo[0];
-				tabuleiro[i][7] = pp->cavalo[1];
-				tabuleiro[i][3] = pp->bispo[0];
-				tabuleiro[i][6] = pp->bispo[1];
-				tabuleiro[i][4] = pp->rainha[0];
-				tabuleiro[i][5] = pp->rei;
+				tabuleiro[i][1] = tolower(pecas_tab[1]);
+				tabuleiro[i][8] = tolower(pecas_tab[1]);
+				tabuleiro[i][2] = tolower(pecas_tab[2]);
+				tabuleiro[i][7] = tolower(pecas_tab[2]);
+				tabuleiro[i][3] = tolower(pecas_tab[3]);
+				tabuleiro[i][6] = tolower(pecas_tab[3]);
+				tabuleiro[i][5] = tolower(pecas_tab[4]);
+				tabuleiro[i][4] = tolower(pecas_tab[5]);
 			}else if(i == 2){
-				tabuleiro[i][j] = pp->peao[j-1];
+				tabuleiro[i][j] = tolower(pecas_tab[0]);
 			}else if(i == 7){
-				tabuleiro[i][j] = pb->peao[j-1];
+				tabuleiro[i][j] = pecas_tab[0];
 			}else if(i == 8){
-				tabuleiro[i][1] = pb->torre[0];
-				tabuleiro[i][8] = pb->torre[1];
-				tabuleiro[i][2] = pb->cavalo[0];
-				tabuleiro[i][7] = pb->cavalo[1];
-				tabuleiro[i][3] = pb->bispo[0];
-				tabuleiro[i][6] = pb->bispo[1];
-				tabuleiro[i][4] = pb->rainha[0];
-				tabuleiro[i][5] = pb->rei;
+				tabuleiro[i][1] = pecas_tab[1];
+				tabuleiro[i][8] = pecas_tab[1];
+				tabuleiro[i][2] = pecas_tab[2];
+				tabuleiro[i][7] = pecas_tab[2];
+				tabuleiro[i][3] = pecas_tab[3];
+				tabuleiro[i][6] = pecas_tab[3];
+				tabuleiro[i][5] = pecas_tab[4];
+				tabuleiro[i][4] = pecas_tab[5];
 			}else{
 				tabuleiro[i][j] = ' ';
 			}
 		}
 	}
 }
-int coordenadas(){
-	
-}
-void inversor(criar tabuleiro, pecas *pb, pecas *pp){
-	criar tabuleiro_temporario;
-	for(int i = 9; i > 0;i--){
-		for(int j = 9; j>0;j--){
-			tabuleiro_temporario[i][j] = tabuleiro[i][j];
+int cont = 0;
+int coordenadas(char coordenada[2]){
+	char num[9] = {' ','1','2','3','4','5','6','7','8'};
+	char letras[9] = {' ','a','b','c','d','e','f','g','h'};
+	int x, y;
+	for(int i = 0;i<9;i++){
+		if(coordenada[0] == letras[i]){
+			x = i;
+		}
+		if(coordenada[1] == num[i]){
+			y = i;
 		}
 	}
-	for(int i = 1; i < 9;i--){
-		for(int j = 1; j<9;j--){
-			tabuleiro[i][j] = tabuleiro_temporario[i][j];
-		}
+	cont += 1;;
+	if(cont % 2 == 0){
+		return x;
+	}else{
+		return y;
 	}
 	
 }
-void partida(criar tabuleiro, pecas *branco, pecas *preto, int saida){
-	char coordenada[2];
-	char jogada[2];
-	enum coordenada_de_letra{
-		vazio,a,b,c,d,e,f,g,h
+int verificador_ganhador(criar tabuleiro){
+	int verificadorb = 0;
+	int verificadorp = 0;
+	for(int i = 0; i<9;i++){
+		for(int j = 0; j<9; j++){
+			if(tabuleiro[i][j] == 'R'){
+				verificadorb = 1;
+			}
+			if(tabuleiro[i][j] == 'r'){
+				verificadorp = 1;
+			}
+		}
+	}
+
+	if(verificadorb == 1 && verificadorp == 0){
+		return 1;
+	}
+	if(verificadorb == 0 && verificadorp == 1){
+		return 2;
+	}else{
+		return 0;
+	}
+}
+/*void inversor(criar tabuleiro){
+	char num[9] = {' ','1','2','3','4','5','6','7','8'};
+	char letras[9] = {' ','a','b','c','d','e','f','g','h'};
+	char temp[8];
+	int inicio = 1;
+	int fim = 7;//tamanho da linha -1
+	
+	while(inicio<fim){
+		for(int i = 1; i<9;i++){
+			temp[i] = tabuleiro[inicio][i];
+			tabuleiro[inicio][i] = tabuleiro[fim][i];
+			tabuleiro[fim][i] = temp[i];
+		}
+		inicio++;
+		fim--;
+	}
+	for(int i = 0; i<9;i++){
+		for(int j = 0; j<9; j++){
+			if(i == 0){
+				tabuleiro[i][j] = letras[j];
+			}
+			if(j == 0){
+				tabuleiro[i][j] = num[i];
+			}
+		}
+	}
+}*/ //esta incompleto
+
+void upgrade_peao(criar tabuleiro){
+	int erro = 0;
+	int peao;
+	char c;
+	char escolha[7];
+	enum pecas_de_troca{
+		torre,
+		cavalo,
+		bispo,
+		rainha
 	};
-	do{
+	while(!erro){
+		erro = 1;
 		for(int i=0; i<9;i++){
-			for(int j=0; j<9;j++){
-				if(i == 0 && j == 0){
-					printf("\t %c  ", tabuleiro[i][j]);
-				}else if( i == 0){
-					printf(" %c  ", tabuleiro[i][j]);
-				}else if(j == 0){
-					printf(" %c \t", tabuleiro[i][j]);
-				}else{
-					printf("[%c] ", tabuleiro[i][j]);
+			if(tabuleiro[1][i] == 'P'){
+				printf("Escreva qual peça ira trocar pelo peao\n");
+				printf("Torre - Cavalo - Bispo - Rainha\n");
+				fgets(escolha, sizeof(escolha),stdin);
+				for(int i=0;i<7;i++){
+					escolha[i] = tolower(escolha[i]);
+				}
+				if(strstr(escolha, "torre")){
+					peao = torre;
+				}else if(strstr(escolha,"cavalo")){
+					peao = cavalo;
+				}else if(strstr(escolha, "bispo")){
+					peao = bispo;
+				}else if(strstr(escolha, "rainha")){
+					peao = rainha;
+				}
+				switch(peao){
+					case torre:
+						tabuleiro[1][i] = 'T';
+						break;
+					case cavalo:
+						tabuleiro[1][i] = 'H';
+						break;
+					case bispo:
+						tabuleiro[1][i] = 'B';
+						break;
+					case rainha:
+						tabuleiro[1][i] = 'Q';
+						break;
+					default:
+						printf("Escolha não reconhecida, Escreva novamente\n");
+						erro = 0;
+						break;
 				}
 			}
-			printf("\n");
+			if(tabuleiro[8][i] == 'p'){
+				printf("Escreva qual peça ira trocar pelo peao\n");
+				printf("Torre - Cavalo - Bispo - Rainha\n");
+				fgets(escolha, sizeof(escolha),stdin);
+				for(int i=0;i<7;i++){
+					escolha[i] = tolower(escolha[i]);
+				}
+				if(strstr(escolha, "torre")){
+					peao = torre;
+				}else if(strstr(escolha,"cavalo")){
+					peao = cavalo;
+				}else if(strstr(escolha, "bispo")){
+					peao = bispo;
+				}else if(strstr(escolha, "rainha")){
+					peao = rainha;
+				}
+				switch(peao){
+					case torre:
+						tabuleiro[8][i] = 't';
+						break;
+					case cavalo:
+						tabuleiro[8][i] = 'h';
+						break;
+					case bispo:
+						tabuleiro[8][i] = 'b';
+						break;
+					case rainha:
+						tabuleiro[8][i] = 'q';
+						break;
+					default:
+						printf("Escolha não reconhecida, Escreva novamente\n");
+						erro = 0;
+						break;
+				}
+			}
+		}
+	}
+	if(c == '\n')//verifica se tem algo no buffer, caso tenha limpa o buffer
+	{
+		clearbuffer();
+	}
+}
+
+void imprimir_tabuleiro(criar tabuleiro){
+	for(int i=0; i<9;i++){
+		for(int j=0; j<9;j++){
+			if(i == 0 && j == 0){
+				printf("\t %c  ", tabuleiro[i][j]);
+			}else if( i == 0){
+				printf(" %c  ", tabuleiro[i][j]);
+			}else if(j == 0){
+				printf(" %c \t", tabuleiro[i][j]);
+			}else{
+				printf("[%c] ", tabuleiro[i][j]);
+			}
 		}
 		printf("\n");
-/*		do{
-			int jogou = 0;
+	}
+	printf("\n");
+}
+int partida(criar tabuleiro){
+	char c;
+	char saida[4];
+	char inicio[3];
+	char destino[3];
+	int ix,iy,dx,dy;
+	int vencedor = 0;
+	do{
+		int jogou = 0;
+		system("clear");
+		imprimir_tabuleiro(tabuleiro);
+		do{
+			if(c == '\n'){
+			clearbuffer();
+		}
 			printf("Insira a coordenada da peça para mover:\n");
-			char deu_certo[] = fgets(coordenada,2,stdin);
-			if(!deu_certo){
-				clearbuffer();
-				jogada[2] = "9b";
+			fgets(inicio, sizeof(inicio), stdin);
+			clearbuffer();
+			printf("Insira a coordenada destino:\n");
+			fgets(destino, sizeof(destino), stdin);
+			clearbuffer();
+			ix = coordenadas(inicio);
+			iy = coordenadas(inicio);
+			dx = coordenadas(destino);
+			dy = coordenadas(destino);
+			if(ix == dx && iy == dy){
+				printf("A peça não foi movida.\n");
+			}else{
+				tabuleiro[dx][dy] = tabuleiro[ix][iy];
+				tabuleiro[ix][iy] = ' ';
+				jogou++;
 			}
-			jogou +=1;
-		}while(jogou <1);*/
-	}while(branco->rei != ' '|| preto->rei != ' ');
-
+			upgrade_peao(tabuleiro);
+		}while(!jogou);
+		//inversor(tabuleiro); // incompleto
+		vencedor = verificador_ganhador(tabuleiro);
+	}while(vencedor == 0);
+	system("clear");
+	imprimir_tabuleiro(tabuleiro);
+	if(c == '\n'){
+		clearbuffer();
+	}
+	if(vencedor == 1){
+		printf("Parabens o jogador das peças brancas ganhou!!!\n");
+		sleep(5);
+		return menu();
+	}else if(vencedor == 2){
+		printf("Parabens o jogador das peças pretas ganhou!!!\n");
+		sleep(5);
+		return menu();
+	}
+	return menu();
 }
 
 void jogo_xadrez (){	
+	printf("\nBEM VINDO AO JOGO DE XADREZ\n\n");
 	int saida = menu();
 	if(saida){
-	pecas pecasb;
-	pecas pecasp;
-	definir_pecas(&pecasb, &pecasp);
 	criar tabuleiro;
-	organizar_tabuleiro(tabuleiro,&pecasb,&pecasp);
+	organizar_tabuleiro(tabuleiro);
 	do{
-		partida(tabuleiro,&pecasb,&pecasp,saida);
+		system("clear");
+		saida = partida(tabuleiro);
+		organizar_tabuleiro(tabuleiro);
 	}while(saida);
+	printf("Obrigado por jogar!!!\n");
 	}else{
 		printf("Até a proxima!!\n");
 	}
